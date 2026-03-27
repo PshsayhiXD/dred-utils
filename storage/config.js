51544763223203e8708821ed227273a92da5a258
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-03-27 18:22:00 +0700

    Removed JSDoc

--- storage/config.js
+++ storage/config.js
@@ -1,27 +1,13 @@
 "use strict";
 
-/**
- * @readonly
- */
 export const defaultConfig = {
   PLUGIN: {}
 };
 
-/**
- * Get config from storage.
- * @async
- * @returns {Promise<Object>} The resolved config object.
- */
 export const getConfig = async () => {
   return await chrome.storage.local.get(defaultConfig);
 };
 
-/**
- * Update config values.
- * @async
- * @param {Object} values The config values to update.
- * @returns {Promise<void>} Resolves after update.
- */
 export const setConfig = async (values) => {
   const current = await getConfig();
   const merged = {
