96e0f7e99b9c3ceb946e335b4a289bf557529303
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-03-27 18:25:28 +0700

    Added social

--- utils/constants.js
+++ utils/constants.js
@@ -1,16 +1,5 @@
-// =======================
-//      DOM ELEMENTS
-// =======================
 export * from "./elements/constants.js";
 
-// =======================
-//        CONSTANTS
-// =======================
-/**
- * Predefined color for ranks.
- * @readonly
- * @type {Object.<string,string>}
- */
 export const colors = {
   owner:"#b388ff", captain:"#55d7ff", crew:"#ffd24d", guest:"#9aa0a6",
   offline:"#7a7f87", banned:"#ff3b3b", online:"#4caf50"
