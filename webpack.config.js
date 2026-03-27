34477b00e1fff585e6d730af53abe0c1170f69b0
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-03-27 18:30:04 +0700

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,21 @@ const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default {
   mode: "production",
+  //devtool: "cheap-module-source-map",
   entry: {
     content: "./content.js",
-    plugins: "./utils/pluginLoader.js",
+    popup: "./popup/popup.js",
+    content_plugin: "./utils/plugins.js",
+    page_plugin: "./utils/pluginLoader.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   output: {
     filename: "[name].js",
