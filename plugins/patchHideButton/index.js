b80751968c389763e7a2abea5c7e1afe7a0a4865
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-03-27 18:20:11 +0700

    Updated to latest bridge + elements

--- plugins/patchHideButton/index.js
+++ plugins/patchHideButton/index.js
@@ -1,7 +1,7 @@
 import { window_dark_container } from "../../utils/constants.js";
-import { addListener } from "../../bridge/pageBridge.js";
+import { onDispatch } from "../../bridge/pageBridge.js";
 
-addListener("domMutated", () => {
+onDispatch("dredutils:windowDark", () => {
   window_dark_container().addEventListener("click", (e) => {
     const btn = e.target.closest("button");
     if (!btn) return;
@@ -10,4 +10,4 @@ addListener("domMutated", () => {
       e.stopImmediatePropagation();
     }
   }, true);
-}, "windowDark");
+});
