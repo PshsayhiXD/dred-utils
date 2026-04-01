f7fad450d37f94480c048db50f23ac674df5acdf
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-04-01 18:13:51 +0700

    .

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
 import { startMutationObserver } from "./bridge/pageMain.js";
-import { 
-  chat_container, motd_container, 
-  server_section, team_menu_container,
-  window_dark_container, shipyard_ships,
-  chat_content
+import {
+  chat_container,
+  motd_container,
+  server_section,
+  team_menu_container,
+  window_dark_container,
+  shipyard_ships,
+  chat_content,
 } from "./utils/constants.js";
 import { getNamespacedFiles } from "./storage/OPFS.js";
 import { waitForElement } from "./utils/elements/dom.js";
@@ -16,7 +19,7 @@ await loadPlugins(PLUGIN_LIST());
 await new Promise((resolve) => onDispatch("dredutils:pluginsReady", resolve));
 
 // LISTENER START
-window.addEventListener("message", async e => {
+window.addEventListener("message", async (e) => {
   if (e.source !== window || !e.data?.type) return;
   const { type, id, token } = e.data;
   const parsedType = type.replace("dredutils:", "");
@@ -24,7 +27,7 @@ window.addEventListener("message", async e => {
   const sendTo = async (type, data) => {
     const r = await chrome.runtime.sendMessage({ type, ...data });
     window.postMessage({ replyTo: id, value: r }, "*");
-  }
+  };
   if (parsedType === "getSession") return sendTo("getSession");
   if (parsedType === "setSession") return sendTo("setSession", { token });
   if (parsedType === "reloadTabs") return sendTo("reloadGameTabs");
@@ -36,7 +39,7 @@ chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
     (async () => {
       try {
         const files = await getNamespacedFiles(msg.namespace);
-        sendResponse(files)
+        sendResponse(files);
       } catch (err) {
         console.error("OPFS Read error:", err);
         sendResponse([]);
@@ -47,19 +50,33 @@ chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
 });
 
 // OBSERVE STARTS
-await waitForElement({ sel: chat_container(), callback: (el) => {
-  dispatch("dredutils:chatContainer", { node: el });
-} });
-await waitForElement({ sel: motd_container, callback: (el) => {
-  dispatch("dredutils:motdContainer", { node: el });
-} });
-await waitForElement({ sel: shipyard_ships(), callback: (el) => {
-  dispatch("dredutils:shipyardShips", { node: el });
-} });
-await waitForElement({ sel: window_dark_container(), callback: (el) => {
-  dispatch("dredutils:windowDark", { node: el });
-} });
+await waitForElement({
+  sel: chat_container(),
+  callback: (el) => {
+    dispatch("dredutils:chatContainer", { node: el });
+  },
+});
+await waitForElement({
+  sel: motd_container,
+  callback: (el) => {
+    dispatch("dredutils:motdContainer", { node: el });
+  },
+});
+await waitForElement({
+  sel: shipyard_ships(),
+  callback: (el) => {
+    dispatch("dredutils:shipyardShips", { node: el });
+  },
+});
+await waitForElement({
+  sel: window_dark_container(),
+  callback: (el) => {
+    dispatch("dredutils:windowDark", { node: el });
+  },
+});
 await startMutationObserver(chat_content(), { from: "chatContent" });
 await startMutationObserver(server_section(), { from: "serverSection" });
-await startMutationObserver(team_menu_container(), { from: "teamMenuContainer" });
+await startMutationObserver(team_menu_container(), {
+  from: "teamMenuContainer",
+});
 // OBSERVE ENDS
